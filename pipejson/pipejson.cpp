1828c60ce44ed198075c14053cfe4f2034a2e5a1
Taras Glek <tglek@mozilla.com>
2013-03-31 23:21:20 -0700

    Multiplexer works

--- pipejson/pipejson.cpp
+++ pipejson/pipejson.cpp
@@ -11,8 +11,95 @@
 
 using namespace std;
 // todo look at rust flatpipes
+
+static long wrotesum = 0;
+static long readsum = 0;
+
+struct FDBuffer {
+  enum Status {
+    READY,
+    BLOCKED,
+    INPUT_EOF
+  };
+
+  int fd;
+  char *line_buffer;
+  size_t line_buffer_size;
+  ssize_t fwd_size;
+  char *outbuf;
+  size_t fwdsofar;
+  pid_t pid;
+  FDBuffer(int fd = -1, pid_t pid = 0):
+    fd(fd), line_buffer(NULL), line_buffer_size(0), fwd_size(0), outbuf(NULL), fwdsofar(0), pid(pid)
+  {
+  }
+  
+  void malloc(size_t size) {
+    line_buffer = (char*)::malloc(size);
+    line_buffer_size = size;
+  }
+
+  Status write_outstanding() {
+    if (!outbuf)
+      return READY;
+    const int todo_bytes = fwd_size - fwdsofar;
+    int wrote = write(fd, outbuf, todo_bytes);
+
+    if (wrote != -1) {
+      wrotesum += wrote;
+      fwdsofar += wrote;
+    }
+    if (wrote == todo_bytes) {
+      outbuf = NULL;
+      fwdsofar = fwd_size = 0;
+      return READY;
+    } 
+    if (wrote == -1) {
+      if (errno == EAGAIN) {
+        //can't write anything else to socket
+      } else {// IO ERROR
+        perror("write");
+        _exit(1);
+      }
+
+    } else if (wrote < todo_bytes) {
+      outbuf += wrote;
+    } else if (wrote > todo_bytes) {
+      fprintf(stderr, "Impossible write()ed more than asked for\n");
+      _exit(1);
+    }
+    return BLOCKED;
+  }
+  // move data from line_buffer into fd
+  // return -1 when EOF
+  // return 0 when fd wont accept any more bytes
+  // return 1 when ready to write more
+  Status fwd(FILE *input) {
+    if (write_outstanding() == BLOCKED)
+      return BLOCKED;
+    
+    fwd_size = getline(&line_buffer, &line_buffer_size, input);
+    // input EOF
+    if (fwd_size == -1)
+      return INPUT_EOF;
+
+    readsum += fwd_size;
+    outbuf = line_buffer;
+    fwdsofar = 0;
+    return write_outstanding();
+  }
+
+  // not using a destructor cos copy constructors make a mess of fd-management
+  void close() {
+    ::close(fd);
+    free(line_buffer);
+    fd = -1;
+  }
+};
+typedef vector<FDBuffer> fd_vector;
+
 void
-select_wait(int fd)
+select_wait(fd_vector &sockets)
 {
   fd_set wrds;
   struct timeval tv;
@@ -20,42 +107,53 @@ select_wait(int fd)
 
   /* Watch stdin (fd 0) to see when it has input. */
   FD_ZERO(&wrds);
-  FD_SET(fd, &wrds);
+  int maxfd = 0;
+  for(fd_vector::iterator it = sockets.begin();it != sockets.end();it++) {
+    if (it->fd == -1)
+      continue;
+    FD_SET(it->fd, &wrds);
+    maxfd = max(it->fd, maxfd);
+  }
+  maxfd++;
 
   /* Wait up to five seconds. */
-  tv.tv_sec = 1;
+  tv.tv_sec = 5;
   tv.tv_usec = 0;
 
-  retval = select(fd+1, NULL, &wrds, NULL, &tv);
+  retval = select(maxfd, NULL, &wrds, NULL, &tv);
   /* Don't rely on the value of tv now! */
 
   if (retval == -1)
     perror("select()");
   else if (retval) {
-    // printf("time to write\n");
-  }else
-    printf("No data within five seconds.\n");
+    //printf("time to write\n");
+  }
 }
 
-
 int main(int argc, char **argv) {
-  vector<int> workers;
-  for(int worker = 0;worker < 3;worker++) {
+  fd_vector workers;
+  fd_vector::iterator pipe_iterator;
+  if (argc < 3) {
+    fprintf(stderr, "Usage; %s <process number> <cmd + args>", argv[0]);
+    _exit(1);
+  }
+  int worker_count = atoi(argv[1]);
+  for(int worker = 0;worker < worker_count;worker++) {
     // multiple of 2
-    int pipes[2];
+    int pipes[2] = {0,0};
     if (pipe(pipes)) {
       perror("pipe");
       _exit(1);
     }
 
-    int cpid = fork();
-    if (cpid == -1) {
+    int pid = fork();
+    if (pid == -1) {
       perror("fork");
       _exit(1);
     }
 
 
-    if (cpid == 0) {    /* Child reads from pipe */
+    if (pid == 0) {    /* Child reads from pipe */
       close(pipes[1]);          /* Close unused write end */
 #define STDIN 0
       if (dup2(pipes[0], STDIN) == -1) {
@@ -63,61 +161,54 @@ int main(int argc, char **argv) {
         _exit(1);
       }
       close(pipes[0]);
-      execv(argv[1], argv+1);
-      perror("execv");
+      execv(argv[2], argv+2);
+      fprintf(stderr, "Failed to launch subprocess: %s\n", argv[2]);
+      _exit(1);
       // process is replaced with child
     }
     // parent
     close(pipes[0]);          /* Close unused read end */    
-    workers.push_back(pipes[1]);
-  }
-  {            /* Parent writes argv[1] to pipe */
-    long wrotesum = 0;
-    long readsum = 0;
-    
-    int outfd = workers[0];
+
+    int outfd = pipes[1];
     int flags = fcntl(outfd, F_GETFL, 0);
-    fcntl(outfd, F_SETFL, flags | O_NONBLOCK);
-
-
-    size_t buf_size = 1024;
-    char *line_buf = (char*)malloc(buf_size);
-    while(true) {
-      ssize_t input_size = getline(&line_buf, &buf_size, stdin);
-      const ssize_t total_input_size = input_size;
-      if (input_size == -1)
-        break;
-      readsum += input_size;
-      int wrotesofar = 0;
-      char *outbuf = line_buf;
-      while(wrotesofar != total_input_size) {
-        int wrote = write(outfd, outbuf, input_size);
-        if (wrote != -1) {
-          wrotesum += wrote;
-          wrotesofar += wrote;
-        }
-        if (wrote != input_size) {
-          //printf("wrote %d/%ld\n", wrote, input_size);
-          if (wrote == -1) {
-            if (errno != EAGAIN)
-              perror("write");
-          }else if (wrote < input_size) {
-            outbuf += wrote;
-            input_size -= wrote;
-          }
-          select_wait(outfd);
-        }
-      }
-      if (readsum != wrotesum)
-        printf("max line buffer size:%lu read:%ld wrote:%ld diff:%d\n", buf_size, readsum, wrotesum, (int)(readsum - wrotesum));
-    }
-    free(line_buf);
-    printf("line size:%lu read:%ld wrote:%ld\n", buf_size, readsum, wrotesum);
-    for(vector<int>::iterator it = workers.begin();it != workers.end(); it++) {
-      close(*it);          /* Reader will see EOF */
-    } 
-    wait(NULL);                /* Wait for child */
-    _exit(0);
+    if (fcntl(outfd, F_SETFL, flags | O_NONBLOCK) == -1) 
+      perror("fcntl");
+    FDBuffer child(outfd, pid);
+    workers.push_back(child);
   }
 
+  pipe_iterator = workers.begin();
+  bool pipes_full = false;
+  while(true) {
+    const int pipe_status = pipe_iterator->fwd(stdin);
+    if (pipe_status == FDBuffer::INPUT_EOF)
+      break;
+    else if (pipe_status == FDBuffer::BLOCKED) {
+      pipe_iterator++;
+      if (pipe_iterator == workers.end()) {
+        pipe_iterator = workers.begin();
+        if (pipes_full) {
+        }
+        pipes_full = true;
+      }
+      select_wait(workers);
+    } else if (pipe_status == FDBuffer::READY) {
+      pipes_full = false;
+      continue;
+    }
+  }
+  for(pipe_iterator = workers.begin();pipe_iterator != workers.end(); pipe_iterator++) {
+    while(pipe_iterator->write_outstanding() == FDBuffer::BLOCKED)
+      select_wait(workers);
+    pipe_iterator->close();
+  } 
+
+
+  while (true) {
+    pid_t done = wait(NULL);
+    if (done == -1) {
+      if (errno == ECHILD) break; // no more child processes
+    }
+  }
+  fprintf(stderr, "read:%ld wrote:%ld diff:%d\n", readsum, wrotesum, (int)(readsum - wrotesum));
 }
