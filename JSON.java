587ec2ed22b5d7a77e3b4848b205ec2c9520c190
Mark Reid <mreid@mozilla.com>
2013-04-02 14:52:24 -0300

    Fix Java test

--- JSON.java
+++ JSON.java
@@ -1,13 +1,24 @@
 import java.io.*;
+import java.util.Date;
 import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
 
 public class JSON {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, JsonParseException {
+    long start = new Date().getTime();
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     JsonFactory factory = new JsonFactory();
+    JsonParser parser = null;
     String s;
     while ((s = in.readLine()) != null && s.length() != 0) {
-      factory.createJsonParser(s).readValueAsTree();
+      parser = factory.createJsonParser(s);
+      while(parser.nextToken() != null) {
+         // do nothing
+      }
     }
+    long end = new Date().getTime();
+
+    System.out.println("Processing took " + (end - start) + "ms");
   }
 }
