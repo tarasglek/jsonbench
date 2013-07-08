b11dc944995ca3818e692b0df9622765fbecdb89
Mark Reid <mreid@mozilla.com>
2013-07-08 17:54:40 -0300

    Update rust benchmark to work with rust 0.7

--- rust.rs
+++ rust.rs
@@ -1,31 +1,27 @@
-extern mod std;
-use core::io;
-use core::str;
-use std::json;
-use std::time;
+extern mod extra;
 
 fn main() {
 
-    let i = io::stdin();
-    let t1 = time::get_time();
+    let i = std::io::stdin();
+    let t1 = extra::time::get_time();
     let mut bytes_read = 0;
     while !i.eof() {
         let line = i.read_line();
-        let json = json::from_str(line);
+        let json = extra::json::from_str(line);
         if line.len() != 0 {
-            bytes_read += str::len(line);
+            bytes_read += line.len();
             match (json) {
                 Ok(value) => {}
                 Err(e) => {
                     let mut tmp = ~"'";
-                    tmp+=(line);
-                    tmp+=~"'";
-                    io::println(~"Error parsing " + tmp);
+                    tmp = tmp + line;
+                    tmp = tmp + ~"'";
+                    std::io::println(~"Error parsing " + tmp);
                 }
             }
         }
     }
-    let t2 = time::get_time();
+    let t2 = extra::time::get_time();
 
     let mut seconds = t2.sec - t1.sec;
     let mut nanos = t2.nsec;
@@ -37,5 +33,5 @@ fn main() {
     nanos -= t1.nsec;
     let megabytes_read = bytes_read as float / 1024f / 1024f;
     let float_seconds = seconds as float + (nanos as float / 1000000000f);
-    io::println(fmt!("Processing %.02f MB took %.02f seconds (%.02f MB/s).", megabytes_read, float_seconds, (megabytes_read / float_seconds)));
+    std::io::println(fmt!("Processing %.02f MB took %.02f seconds (%.02f MB/s).", megabytes_read, float_seconds, (megabytes_read / float_seconds)));
 }
