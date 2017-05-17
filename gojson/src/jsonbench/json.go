cf4de3badc6140beb8be20cdec0f6ee594c1a177
Olivier Mengué <dolmen@cpan.org>
2017-05-17 22:31:38 +0200

    Reformat Go code using gofmt

--- gojson/src/jsonbench/json.go
+++ gojson/src/jsonbench/json.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-   "encoding/json"
-   "fmt"
-   "io"
-   "os"
-   "log"
-   "time"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
 )
 
 func main() {
-   if len(os.Args) < 2 {
-      log.Fatal("Usage: %s inputfile\n", os.Args[0])
-   }
-   file, err := os.Open(os.Args[1])
-   if err != nil {
-      log.Printf("Usage: %s inputfile\n", os.Args[0])
-      log.Fatal(err)
-   }
-   dec := json.NewDecoder(file)
-   startTime := time.Now()
-   for {
-      var m map[string]interface{}
-      if err := dec.Decode(&m); err == io.EOF {
-         break
-      } else if err != nil {
-         log.Fatal(err)
-      }
-   }
-   duration := time.Since(startTime)
-   fileInfo, err := file.Stat()
-   if err != nil {
-      log.Fatal(err)
-   }
-   speed := float32(fileInfo.Size()) / 1024.0 / 1024.0 / float32(duration.Seconds())
-   fmt.Printf("%.02f MB/s in %s", speed, duration)
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: %s inputfile\n", os.Args[0])
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Printf("Usage: %s inputfile\n", os.Args[0])
+		log.Fatal(err)
+	}
+	dec := json.NewDecoder(file)
+	startTime := time.Now()
+	for {
+		var m map[string]interface{}
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+	}
+	duration := time.Since(startTime)
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	speed := float32(fileInfo.Size()) / 1024.0 / 1024.0 / float32(duration.Seconds())
+	fmt.Printf("%.02f MB/s in %s", speed, duration)
 }
