9eac7bb26387d196afbf06da3d41a9c656bc1b30
Olivier Mengué <dolmen@cpan.org>
2017-05-17 23:02:15 +0200

    node.js: refactor
    Move measurement code out of the processing code.

--- node.js
+++ node.js
@@ -1,7 +1,5 @@
 var fs = require('fs');
-var start = new Date()
-var bytes = 0;
-function readLines(input, func) {
+function readLines(input, onLine, onEnd) {
   var remaining = '';
 
   input.on('data', function(data) {
@@ -10,27 +8,32 @@ function readLines(input, func) {
     while (index > -1) {
       var line = remaining.substring(0, index);
       remaining = remaining.substring(index + 1);
-      func(line);
+      onLine(line);
       index = remaining.indexOf('\n');
     }
   });
 
   input.on('end', function() {
     if (remaining.length > 0) {
-      func(remaining);
+      onLine(remaining);
     }
-             var mseconds = (new Date() - start)
-             console.log(1000*bytes/1024/1024/mseconds + "MB/s "+bytes+"bytes in "+mseconds/1000+"s" )
-
+    onEnd();
   });
 }
 
 
-function func(data) {
+var start = new Date();
+var bytes = 0;
+
+function processLine(data) {
   bytes += data.length
   JSON.parse(data)
 }
 
 var input = fs.createReadStream(process.argv[2]);
-readLines(input, func);
 
+
+readLines(input, processLine, function() {
+  var mseconds = (new Date() - start)
+  console.log(1000*bytes/1024/1024/mseconds + "MB/s "+bytes+"bytes in "+mseconds/1000+"s" )
+});
